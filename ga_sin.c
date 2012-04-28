b457743c77796461b18a4b892482b34389526b9b
Mingda Qiu <qiu.mingda.pku@gmail.com>
2012-04-28 14:09:10 +0800

    Rewrite part of ga_sin.c

--- ga_sin.c
+++ ga_sin.c
@@ -11,15 +11,17 @@
 
 #ifndef uint
 typedef unsigned int uint;
+#define UINT_BITSIZE 8*sizeof(uint)
 #endif
 
 // Problem specific arguments
 #define ARGLEN		3 // three parameters
 #define ARGACC		3 // accuracy
-#define ARGMAX		20
-#define ARGMIN		-20
-#define POP_SIZE	20	
-#define ARGSCALE	0.1
+#define ARGMAX		0xffff	
+#define ARGMIN		-0xffff
+#define POP_SIZE	0x60	
+#define ARGSCALE	0.01
+
 
 #define ELITRATE	0.1
 #define MUTATIONRATE 0.25
@@ -39,6 +41,23 @@ typedef struct tagDataPoints
 	int size;
 } DataPoints;
 
+void debug_print_genome(FILE * fOut, uint * genome)
+{
+	int i,k;	
+	char map[3] = {'A', 'B', 'C'};
+	for(k = 0;k < ARGLEN; k++)
+	{
+		fprintf(fOut, "%c: ", map[k]);
+		for(i = 31; i >= 0; i--)
+		{
+			fprintf(fOut, "%d", (genome[k] >> i) & 0x01u);
+		}
+		fprintf(fOut, "\n");
+	}
+}
+
+
+
 /**
  * function y = Asin(Bx + C)
  */
@@ -50,10 +69,10 @@ double target_function(double x, double A, double B, double C)
 void genome_translator(Genome * genome, double * pA, double * pB, double * pC)
 {
 	int *p;
-	p = (int*)genome->genome;
-	*pA = p[1]*ARGSCALE;
-	*pB = p[2]*ARGSCALE;
-	*pC = p[3]*ARGSCALE;
+	p = (int*)(genome->genome);
+	*pA = p[0]*ARGSCALE;
+	*pB = p[1]*ARGSCALE;
+	*pC = p[2]*ARGSCALE;
 }
 
 double square(double a)
@@ -61,19 +80,40 @@ double square(double a)
 	return a*a;
 }
 
+int sort_double_func(const void * d1, const void * d2)
+{
+	return (*((double*)d1)) > (*((double*)d2));
+}
+
+
 void cal_fitness(Genome * population, DataPoints * pdp) 
 {
 	double A,B,C;	
 	int i,j,k;
+	double * fitarr;
+	fitarr = (double*)malloc(pdp->size * sizeof(double));
+
 	for(i = 0; i < POP_SIZE; i++)
 	{
 		population[i].fitness = 0;
 		genome_translator(population+i, &A, &B, &C);
+		memset(fitarr, 0, pdp->size * sizeof(double));
 		for(k = 0; k < pdp->size; k++)
 		{
-			population[i].fitness += square(pdp->y[k] - target_function(pdp->x[k], A,B,C));
+			fitarr[k] = square(pdp->y[k] - target_function(pdp->x[k], A,B,C));
+			//fprintf(stderr, "A,B,C=%.3lf %.3lf %.3lf;x=%d; delta=%.3lf\n", A,B,C, pdp->x[k],
+		//		fitarr[k]);
 		}
+		qsort(fitarr, pdp->size, sizeof(double), sort_double_func);
+		//fprintf(stderr, "\nfitarr %.3lf %.3lf %.3lf: ", A, B, C);
+		//for(j = 0; j < pdp->size; j++)
+		//{
+			//fprintf(stderr, "%lf ", fitarr[j]);
+		//}
+		//fprintf(stderr, "\n");
+		population[i].fitness = fitarr[pdp->size-1];
 	}
+	free(fitarr);
 }
 
 /**
@@ -116,7 +156,7 @@ void elitism(Genome * population, Genome * beta_population, int esize)
 	for(i = 0; i < esize; i++)
 	{
 		memcpy(beta_population[i].genome, population[i].genome,
-			sizeof(sizeof(uint)*ARGLEN));
+			sizeof(uint)*ARGLEN);
 	}
 }
 
@@ -132,13 +172,13 @@ void mutate(Genome * member)
 	int apos;
 	int * p;
 
-	tsize = 8;
+	//tsize = 8;
 	nom = rand() % 6;
 	for(i = 0; i < nom; i++)
 	{
-		apos = rand()%tsize;	
-		k = rand()%3;
-		member->genome[k] ^= 1 << apos;
+		apos = rand()%UINT_BITSIZE;	
+		k = rand()%ARGLEN;
+		member->genome[k] ^= (1 << apos);
 	}
 }
 
@@ -149,18 +189,20 @@ void mutate(Genome * member)
 
 void mate(Genome * population, Genome * beta_population)
 {
-	int esize = POP_SIZE * ELITRATE;
 
 	////
-	elitism(population, beta_population, esize);
 	int m, i, k, i1,i2,pos,tsize;
+	int esize;
+
 	uint tmp;
 	m = esize;
 	i1 = -1;
 	i2 = -1;
 	pos = -1;
-	tsize = sizeof(uint);
+	esize = POP_SIZE * ELITRATE;
+	//tsize = sizeof(uint);
 
+	elitism(population, beta_population, esize);
 
 
 	// crossover
@@ -168,8 +210,8 @@ void mate(Genome * population, Genome * beta_population)
 	{
 		i1 = rand() % POP_SIZE;
 		i2 = rand() % POP_SIZE;
-		k = rand()  % 3;
-		pos = rand() % sizeof(uint);
+		k = rand()  % ARGLEN;
+		pos = rand() % (UINT_BITSIZE);
 
 		for(i = 0; i < k; i++)
 		{
@@ -181,16 +223,15 @@ void mate(Genome * population, Genome * beta_population)
 			beta_population[m].genome[i] =
 				population[i2].genome[i];
 		}
-		tmp = 0;
-		for(i = 0; i <= k; i++)
+		for(i = 0; i <= pos; i++)
 		{
 			beta_population[m].genome[k] |= 
-				(population[i1].genome[k] & (0x01 << k));
+				(population[i1].genome[k] & (0x01 << i));
 		}
-		for(i = k+1; i < tsize; i++)
+		for(i = pos+1; i < UINT_BITSIZE; i++)
 		{
 			beta_population[m].genome[k] |= 
-				(population[i2].genome[k] & (0x01 << k));
+				(population[i2].genome[k] & (0x01 << i));
 		}
 
 		// mutation if necessary
@@ -209,6 +250,7 @@ int sort_func(const void * e1, const void * e2)
 	return (((Genome*)e1)->fitness > ((Genome*)e2)->fitness);
 }
 
+
 void sort_by_fitness(Genome * population)
 {
 	qsort(population, POP_SIZE, sizeof(Genome), sort_func);
@@ -222,8 +264,8 @@ int main(int argc, char ** argv)
 	int * p;
 	int generation;
 
-	int x[] = {0, 1, 2};
-	int y[] = {0, 1, 0};
+	int x[] = {0, 10, 20};
+	int y[] = {0, 10, 0};
 	DataPoints dp;
 	dp.x = x;
 	dp.y = y;
@@ -241,20 +283,21 @@ int main(int argc, char ** argv)
 
 	init_population(population,beta_population);
 
-	for(generation = 0; generation < 100; generation++)
+	for(generation = 0; generation < 1000; generation++)
 	{
 		fprintf(stderr, "Generation %d\n", generation);
 		cal_fitness(population, &dp);
 		sort_by_fitness(population);
 
 		// display
-		//i = 0;
-		for(i = 0; i < POP_SIZE; i ++)
+		i = 0;
+		//for(i = 0; i < POP_SIZE; i ++)
 		{
-			//fprintf(stderr, "Geneation %d\tGenome %d\n", generation, i);
+			fprintf(stderr, "Geneation %d\tGenome %d\n", generation, i);
 
 			genome_translator(population + i, &A, &B, &C);
-			fprintf(stderr, "%02d: %.2lf, %.2lf, %.2lf", i, A,B,C);	
+			fprintf(stderr, "%02d: %.5lf, %.5lf, %.5lf", i, A,B,C);	
+			//debug_print_genome(stderr, population[i].genome);
 			fprintf(stderr, "\t\t...Fitness: %lf\n", 
 				population[i].fitness);
 		}
